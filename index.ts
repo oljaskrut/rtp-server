8ed56f83b5327fe5ab8725d1bdfbee23aa6226c2
Olzhas <oljaskrutf@gmail.com>
2025-02-24 17:44:15 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,19 @@
+// src/index.ts
+
+import { AudioWebSocketServer } from "./src/audio-ws"
+
+// Используйте переменные окружения или задайте значения по умолчанию
+const WS_PORT: number = 8081
+const UDP_LISTEN: string = "127.0.0.1:9999"
+// Если хотите записывать аудио в файл, укажите путь, иначе оставьте пустой строкой
+const WRITE_TO_FILE: string = ""
+// Если формат аудио требует преобразования байтов, установите swap16 в true
+const SWAP16 = false
+
+const server = new AudioWebSocketServer(WS_PORT, UDP_LISTEN, WRITE_TO_FILE, SWAP16)
+
+process.on("SIGINT", () => {
+  console.log("Завершение работы...")
+  server.close()
+  process.exit()
+})
